244fd08b147b265a81d12dd2fec081a8c30c1f1d
Dakota M <dakota.paige@hotmail.com>
2018-05-03 10:13:31 -0700

    Started adding basic http authetication

--- index.js
+++ index.js
@@ -10,16 +10,45 @@ const router = express.Router();
 
 app.use(express.static('public'));
 
+// set up our /items route handlers
+app.use('/items', router);
+
+const auth = (req, res, next) => {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        res.sendStatus(401);
+    }
+
+    let authHeader = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = new Buffer(authHeader, 'base64')
+        .toString()
+        .split(':');
+
+    if (login === 'cat' && password === 'cat') {
+        next();
+    } else {
+        return unauthorized(res);
+    }
+};
+
+app.get('/', auth, (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 router
-    .route('/')
+    .route('/items')
     .get(function(request, response) {
         response.send(items);
     })
     // Handling of how POST requests work to the /items route
-    .post(bodyParser.json(), function(request, response) {
+    .post(bodyParser.urlencoded({ extended: true }), function(
+        request,
+        response
+    ) {
+        console.log('Form values:', JSON.stringify(request.body, null, 2));
         const newItem = request.body;
         items.push(newItem);
-        response.status(201).json(newItem);
+        response.status(201).redirect('/');
 
         console.log(newItem);
 
@@ -27,8 +56,10 @@ router
         // And also send the new quote as JSON in the response
     });
 
-// set up our /items route handlers
-app.use('/items', router);
+// app.post('/items', bodyParser.urlencoded({ extended: true }), (req, res) => {
+//     console.log('Form values:', JSON.stringify(req.body, null, 2));
+//     res.status(200).redirect('/');
+// });
 
 app.listen(port, function() {
     console.log(`Server running @ localhost:${port}`);
